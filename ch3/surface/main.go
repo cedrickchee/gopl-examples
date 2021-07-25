1f0471c7abdddb12b435a7266f8ae5f8b2100ee2
Cedric Chee <cedric@invictusbyte.com>
2021-07-25 15:33:25 +0800

    add surface example

---
+++ ch3/surface/main.go
@@ -0,0 +1,72 @@
+// Surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	// The explanation of how the program works requires only basic geometry.
+	// The essence of the program is mapping between three different coordinate
+	// systems.
+
+	// svg := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	// 	"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+	// 	"width='%d' height='%d'>", width, height)
+	svg := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			// svg += fmt.Sprintf("<polygon points='%g,%g,%g,%g %g,%g,%g,%g' />\n",
+			// 	ax, ay, bx, by, cx, cy, dx, dy)
+			svg += fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	svg += fmt.Sprintln("</svg>")
+
+	http.HandleFunc("/surface", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		fmt.Fprint(w, svg)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// corner returns two values, the coordinates of the corner of the cell.
+func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(i)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
