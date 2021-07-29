ed7503f2893f27e60b438c4564903bae8b827136
Cedric Chee <cedric@invictusbyte.com>
2021-07-29 19:43:00 +0800

    update code samples for chapter 5

--- ch5/various/main.go
+++ ch5/various/main.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -16,6 +17,13 @@ func main() {
 	multipleReturnValues()
 
 	// =========================================================================
+	functionValues()
+
+	// =========================================================================
+	zeroValueFunctionType()
+
+	// =========================================================================
+	functionValuesBehavior()
 }
 
 func declarations() {
@@ -71,3 +79,44 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 func countWordsAndImages(n *html.Node) (words, images int) {
 	return rand.Int() % 100, rand.Int() % 100
 }
+
+func functionValues() {
+	// Functions are first-class values in Go.
+	fmt.Println("square(2) =", square(2))
+	fmt.Println("negative(5) =", negative(5))
+	fmt.Println("product(3,5) =", product(3, 5))
+
+	f := square
+	fmt.Println("f(3) =", f(3)) // "9"
+	f = negative
+	fmt.Println("f(3) =", f(3)) // "-3"
+	fmt.Printf("%T\n", f)       // "func(int) int"
+	/*
+		f = product                 // compile error: cannot use product (value of type func(m int, n int) int) as func(n int) int value in assignment
+	*/
+}
+func square(n int) int     { return n * n }
+func negative(n int) int   { return -n }
+func product(m, n int) int { return m * n }
+
+func zeroValueFunctionType() {
+	// The zero value of a function type is nil. Calling a nil function value
+	// causes a panic.
+	var f func(int) int
+	/*
+		f(3) // panic: runtime error: invalid memory address or nil pointer dereference
+	*/
+
+	// Function values may be compared with `nil`, but they are not comparable,
+	// so they may not be compared against each other or used as keys in a map.
+	if f != nil {
+		f(3)
+	}
+}
+
+func functionValuesBehavior() {
+	fmt.Println(strings.Map(add1, "HAL-9000")) // "IBM.:111"
+	fmt.Println(strings.Map(add1, "VMS"))      // "WNT"
+	fmt.Println(strings.Map(add1, "Admix"))    // "Benjy"
+}
+func add1(r rune) rune { return r + 1 }
