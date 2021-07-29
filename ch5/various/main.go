c7937227e829cf4b03b65e65bb5fc5d8dd1cfa27
Cedric Chee <cedric@invictusbyte.com>
2021-07-29 11:44:10 +0800

    update code samples for chapter 5

--- ch5/various/main.go
+++ ch5/various/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
 	declarations()
+
+	// =========================================================================
+	multipleReturnValues()
 
 	// =========================================================================
 }
@@ -31,4 +41,33 @@ func declarations() {
 		}
 		fmt.Println("factorial(7) =", fac(7))
 	}
+}
+
+func multipleReturnValues() {
+	// Bare return
+	words, images, err := CountWordsAndImages("https://golang.org")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "countWordsAndImages failed: %v", err)
+	}
+	fmt.Printf("%d words and %d images\n", words, images)
+}
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	return rand.Int() % 100, rand.Int() % 100
 }
