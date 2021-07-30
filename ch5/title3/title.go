f289e23b729bfd04cb6c8e98e77fd6ac258e07ec
Cedric Chee <cedric@invictusbyte.com>
2021-07-30 20:12:57 +0800

    add title3 example

---
+++ ch5/title3/title.go
@@ -0,0 +1,107 @@
+// Title3 prints the title of an HTML document specified by a URL.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Copied from gopl.io/ch5/outline2.
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			// no panic
+		case bailout{}:
+			// "expected" panic
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p) // unexpected panic; carry on panicking
+		}
+	}()
+
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{}) //multiple title elements
+			}
+			title = n.FirstChild.Data
+		}
+	}
+
+	// Bail out of recursion if we find more than one non-empty title.
+	forEachNode(doc, visitNode, nil)
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+	return title, nil
+}
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check Content-Type is HTML (e.g., "text/html; charset=utf-8").
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	// Print doc's title element.
+	title, err := soleTitle(doc)
+	if err != nil {
+		return err
+	}
+	fmt.Println(title)
+	return nil
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		if err := title(arg); err != nil {
+			fmt.Fprintf(os.Stderr, "title: %v", err)
+		}
+	}
+}
+
+/*
+Run:
+$ go run gopl.io/ch5/title3 https://golang.org
+The Go Programming Language
+
+$ go run gopl.io/ch5/title3 https://golang.org/doc/effective_go.html
+Effective Go - The Go Programming Language
+
+$ go run gopl.io/ch5/title3 https://github.com
+GitHub: Where the world builds software · GitHub
+
+$ go run gopl.io/ch5/title3 https://golang.org/doc/gopher/frontpage.png
+title: https://golang.org/doc/gopher/frontpage.png has type image/png, not text/html
+*/
