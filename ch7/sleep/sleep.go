2c41e4a3acc6c47f993907651a336c987393144b
Cedric Chee <cedric@invictusbyte.com>
2021-08-02 16:21:40 +0800

    add sleep example

---
+++ ch7/sleep/sleep.go
@@ -0,0 +1,35 @@
+// The sleep program sleeps for a specified period of time.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var period = flag.Duration("period", 1*time.Second, "sleep period")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v...", *period)
+	time.Sleep(*period)
+	fmt.Println()
+}
+
+/*
+Run:
+$ go build gopl.io/ch7/sleep
+$ ./sleep
+Sleeping for 1s...
+
+$ ./sleep -period 2s
+Sleeping for 2s...
+$ ./sleep -period 50ms
+Sleeping for 50ms...
+$ ./sleep -period 2m30s
+Sleeping for 2m30s...
+$ ./sleep -period 1.5h
+Sleeping for 1h30m0s...
+$ ./sleep -period "1 day"
+invalid value "1 day" for flag -period: time: invalid duration 1 day
+*/
