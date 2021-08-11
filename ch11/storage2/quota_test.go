73a8d048a7a36f6415acd1c51f5e815c7fe66480
Cedric Chee <cedric@invictusbyte.com>
2021-08-11 23:21:28 +0800

    add storage2 example

---
+++ ch11/storage2/quota_test.go
@@ -0,0 +1,55 @@
+package storage
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+	var notifiedUser, notifiedMsg string
+	notifyUser = func(user, msg string) {
+		notifiedUser, notifiedMsg = user, msg
+	}
+
+	const user = "joe@example.org"
+	usage[user] = 980000000 // simulate a 980MB-used condition
+	CheckQuota(user)
+	if notifiedUser == "" && notifiedMsg == "" {
+		t.Fatalf("notifyUser not called")
+	}
+	if notifiedUser != user {
+		t.Errorf("wrong user (%s) notified, want %s",
+			notifiedUser, user)
+	}
+	const wantSubstring = "98% of your quota"
+	if !strings.Contains(notifiedMsg, wantSubstring) {
+		t.Errorf("Unexpected notification message <<%s>>, "+
+			"want substring %q", notifiedMsg, wantSubstring)
+	}
+}
+
+/*
+Run test:
+go test -v gopl.io/ch11/storage2
+=== RUN   TestCheckQuotaNotifiesUser
+--- PASS: TestCheckQuotaNotifiesUser (0.00s)
+PASS
+ok   gopl.io/ch11/storage2      0.003s
+*/
+
+/*
+//!+defer
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+	// Save and restore original notifyUser.
+	saved := notifyUser
+	defer func() { notifyUser = saved }()
+
+	// Install the test's fake notifyUser.
+	var notifiedUser, notifiedMsg string
+	notifyUser = func(user, msg string) {
+		notifiedUser, notifiedMsg = user, msg
+	}
+	// ...rest of test...
+}
+//!-defer
+*/
