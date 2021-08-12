c064b5e5175f3ed7ed9f3ff3bb4adf6633e0e7c1
Cedric Chee <cedric@invictusbyte.com>
2021-08-12 16:07:09 +0800

    add benchmarks & performance optimizations

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -6,13 +6,21 @@ import "unicode"
 // IsPalindrome reports whether s reads the same forward and backward.
 // Letter case is ignored, as are non-letters.
 func IsPalindrome(s string) bool {
-	var letters []rune
+	// Performance optimization 2:
+	// Pre-allocate a sufficiently large array for use by letters, rather than
+	// expand it by successive calls to append.
+	// var letters []rune // previously
+	letters := make([]rune, 0, len(s)) // now
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
 		}
 	}
-	for i := range letters {
+	// Performance optimization 1:
+	// Make the loop stop checking at the midpoint, to avoid doing each
+	// comparison twice.
+	n := len(letters) / 2
+	for i := 0; i < n; i++ {
 		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -26,3 +26,67 @@ func TestIsPalindrome(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	// A benchmark for `IsPalindrome` that calls it `N` times in a loop.
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
+
+/*
+We run the benchmark with the command below.
+$ cd $GOPATH/src/gopl.io/ch11/word2
+$ go test -bench=.
+goos: linux
+goarch: amd64
+pkg: gopl.io/ch11/word2
+cpu: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
+BenchmarkIsPalindrome-8          2261197               523.7 ns/op
+PASS
+ok   gopl.io/ch11/word2 1.725s
+*/
+
+/*
+Performance Optimization 1:
+
+Now that we have a benchmark and tests, it’s easy to try out ideas for making
+the program faster. Perhaps the most obvious optimization is to make
+`IsPalindrome`'s second loop stop checking at the midpoint, to avoid doing each
+comparison twice.
+
+But as is often the case, an obvious optimization doesn’t always yield the
+expected benefit. This one delivered a mere 4% improvement in one experiment.
+
+$ go test -bench=. gopl.io/ch11/word2
+goos: linux
+goarch: amd64
+pkg: gopl.io/ch11/word2
+cpu: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
+BenchmarkIsPalindrome-8          2543506               482.0 ns/op
+PASS
+ok   gopl.io/ch11/word2 1.710s
+
+$ go test -bench=. -benchmem gopl.io/ch11/word2
+goos: linux
+goarch: amd64
+pkg: gopl.io/ch11/word2
+cpu: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
+BenchmarkIsPalindrome-8          2434116               487.5 ns/op           248 B/op          5 allocs/op
+PASS
+ok   gopl.io/ch11/word2 1.692s
+
+and after performance optimization 2:
+
+$ go test -bench=. -benchmem gopl.io/ch11/word2
+goos: linux
+goarch: amd64
+pkg: gopl.io/ch11/word2
+cpu: Intel(R) Core(TM) i7-1065G7 CPU @ 1.30GHz
+BenchmarkIsPalindrome-8          4855659               254.8 ns/op           128 B/op          1 allocs/op
+PASS
+ok   gopl.io/ch11/word2 1.493s
+*/
+
+// Consolidating the allocations in a single call to `make` eliminated 75% of
+// the allocations and halved the quantity of allocated memory.
